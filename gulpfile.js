7b6e0a0e924521d38618745538fa70e76ec74b10
denzelwash <denzelwash33@mail.ru>
2021-08-16 12:32:36 +0300

    add js task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
+const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const postcss = require('gulp-postcss')
@@ -17,14 +18,14 @@ const browserSync = require('browser-sync').create();
 	// del = require('del'),
 	// imagemin = require('gulp-imagemin'),
 	// plumber = require('gulp-plumber'),
-	// rename = require("gulp-rename"),
+	// rename = require('gulp-rename'),
 	// sourcemaps = require('gulp-sourcemaps'),
 	// browserSync = require('browser-sync').create();
 
 //////////////////////////////////////////////
 
 function buildStyles() {
-  return src('./assets/sass/*.scss')
+  return src('assets/sass/*.scss')
 		.pipe(sourcemaps.init())
     .pipe(sass()
 		.on('error', sass.logError))
@@ -32,19 +33,30 @@ function buildStyles() {
     .pipe(cleanCSS())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(sourcemaps.write('/'))
-    .pipe(dest('./assets/css'))
+    .pipe(dest('assets/css'))
+		.pipe(browserSync.stream());
+};
+
+function jsLibs() {
+	return src(['assets/js/libs/jquery.js', 'assets/js/libs/*.js'], {allowEmpty: true})
+		.pipe(concat('libs.js'))
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(dest('assets/js/'))
 		.pipe(browserSync.stream());
 };
 
 function server() {
 	browserSync.init({
 		server: {
-				baseDir: "./"
+				baseDir: './'
 		}
 	});
-	watch('./assets/sass/**/*.scss', buildStyles);
-	watch("./*.html").on('change', browserSync.reload);
+	watch('assets/sass/**/*.scss', buildStyles);
+	watch('assets/js/libs/**/*.js', jsLibs);
+	watch('*.html').on('change', browserSync.reload);
+	watch('assets/js/*.js').on('change', browserSync.reload);
 }
 
 exports.scss = buildStyles;
-exports.watch = series(buildStyles, server);
+exports.watch = series(buildStyles, jsLibs, server);
