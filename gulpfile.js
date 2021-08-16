783af4099631b881c8750b4c5175e90d6bba97a6
denzelwash <denzelwash33@mail.ru>
2021-08-16 14:10:12 +0300

    add images task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, parallel, series, watch } = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
@@ -6,23 +6,9 @@ const autoprefixer = require('autoprefixer');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
-const postcss = require('gulp-postcss')
+const postcss = require('gulp-postcss');
+const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
-
-	// pug = require('gulp-pug'),
-	// prefixer = require('gulp-autoprefixer'),
-	// uglify = require('gulp-uglify'),
-	// concat = require('gulp-concat'),
-	// cssnano = require('gulp-cssnano'),
-	// sass = require('gulp-sass'),
-	// del = require('del'),
-	// imagemin = require('gulp-imagemin'),
-	// plumber = require('gulp-plumber'),
-	// rename = require('gulp-rename'),
-	// sourcemaps = require('gulp-sourcemaps'),
-	// browserSync = require('browser-sync').create();
-
-//////////////////////////////////////////////
 
 function buildStyles() {
   return src('assets/sass/*.scss')
@@ -37,7 +23,7 @@ function buildStyles() {
 		.pipe(browserSync.stream());
 };
 
-function jsLibs() {
+function buildJsLibs() {
 	return src(['assets/js/libs/jquery.js', 'assets/js/libs/*.js'], {allowEmpty: true})
 		.pipe(concat('libs.js'))
 		.pipe(uglify())
@@ -46,6 +32,21 @@ function jsLibs() {
 		.pipe(browserSync.stream());
 };
 
+function buildImages() {
+	return src('assets/img/**/*.{png,jpg,svg}')
+		.pipe(imagemin([
+			imagemin.mozjpeg({quality: 80, progressive: true}),
+			imagemin.optipng({optimizationLevel: 2}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: true},
+					{cleanupIDs: false}
+				]
+			})
+		]))
+		.pipe(dest('assets/img/'));
+}
+
 function server() {
 	browserSync.init({
 		server: {
@@ -53,10 +54,13 @@ function server() {
 		}
 	});
 	watch('assets/sass/**/*.scss', buildStyles);
-	watch('assets/js/libs/**/*.js', jsLibs);
+	watch('assets/js/libs/**/*.js', buildJsLibs);
 	watch('*.html').on('change', browserSync.reload);
+	watch('assets/img/**/*').on('all', browserSync.reload);
 	watch('assets/js/*.js').on('change', browserSync.reload);
 }
 
 exports.scss = buildStyles;
-exports.watch = series(buildStyles, jsLibs, server);
+exports.jsLibs = buildJsLibs;
+exports.images = buildImages;
+exports.watch = series(buildStyles, buildJsLibs, server);
